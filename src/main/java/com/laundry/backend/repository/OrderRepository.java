b2b4a0b782395ccab6fd1a395bc98ff536b8c3fc
Kawya Dissanayaka <kawyadissanayaka95@gmail.com>
2025-12-13 16:11:46 +0530

    Initial Commit

--- src/main/java/com/laundry/backend/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.laundry.backend.repository;
-
-import com.laundry.backend.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface OrderRepository extends JpaRepository<Order, String> {
-    List<Order> findByUserId(String userId);
-}
